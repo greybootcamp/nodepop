62b08585141b6ee6ccc2b1e87d4542e9dece6599
greybootcamp <gastonrey@gmail.com>
2018-02-13 19:03:53 +0100

    Added a new import JSON and fixed the populate script to close() connection when it's done

--- bin/populate.js
+++ bin/populate.js
@@ -4,90 +4,12 @@ const mongoose = require("mongoose");
 const conn = mongoose.connection;
 const Anuncio = require("../models/Anuncio");
 
-const anuncios = [
-  {
-    name: "Bicicleta",
-    selling: true,
-    price: 230.15,
-    photo: "bici.jpg",
-    tags: ["lifestyle", "motor"]
-  },
-  {
-    name: "iPhone 3GS",
-    selling: false,
-    price: 50.0,
-    photo: "iphone.png",
-    tags: ["lifestyle", "mobile"]
-  },
-  {
-    name: "iPhone X",
-    selling: true,
-    price: 5000.0,
-    photo: "iphonex.png",
-    tags: ["trendi", "postureo", "mobile"]
-  },
-  {
-    name: "Renault Megane",
-    selling: true,
-    price: 8000.0,
-    photo: "renault_megane.png",
-    tags: ["lifestyle", "coche", "coupe", "2.0"]
-  },
-  {
-    name: "MacBook Pro 11'",
-    selling: false,
-    price: 2500.87,
-    photo: "macbook_pro.png",
-    tags: ["macbook", "notebook", "2017"]
-  },
-  {
-    name: "iPhone 3GS",
-    selling: false,
-    price: 50.0,
-    photo: "iphone.png",
-    tags: ["lifestyle", "mobile"]
-  },
-  {
-    name: "iPhone 3GS",
-    selling: false,
-    price: 50.0,
-    photo: "iphone.png",
-    tags: ["lifestyle", "mobile"]
-  },
-  {
-    name: "iPhone 3GS",
-    selling: false,
-    price: 50.0,
-    photo: "iphone.png",
-    tags: ["lifestyle", "mobile"]
-  },
-  {
-    name: "iPhone 3GS",
-    selling: false,
-    price: 50.0,
-    photo: "iphone.png",
-    tags: ["lifestyle", "mobile"]
-  },
-  {
-    name: "iPhone 3GS",
-    selling: false,
-    price: 50.0,
-    photo: "iphone.png",
-    tags: ["lifestyle", "mobile"]
-  },
-  {
-    name: "iPhone 3GS",
-    selling: false,
-    price: 50.0,
-    photo: "iphone.png",
-    tags: ["lifestyle", "mobile"]
-  }
-];
+const anuncios = require("./MOCK_DATA.json");
 
-conn.once("open", () => {
+conn.once("open", async () => {
   console.log("MongoDB Already Connected...", mongoose.connection.name);
   // Drop any existing data
-  mongoose.connection.db.dropDatabase(function(err, result) {
+  await mongoose.connection.db.dropDatabase(function(err, result) {
     if (err) {
       console.log(err);
     }
@@ -95,14 +17,14 @@ conn.once("open", () => {
   });
 
   // Go through each anuncio
-  anuncios.map(data => {
+  anuncios.map(async data => {
     // Initialize a model with anuncios data
     const anuncio = new Anuncio(data);
     // and save it into the database
-    anuncio.save();
+    await anuncio.save();
+    console.log("Data Saved!!");
+    mongoose.connection.close();
   });
-
-  return true;
 });
 
 // Connect to MongoDB
