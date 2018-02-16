44e3c95d124593f2bf17fbda33573d988dc64546
greybootcamp <gastonrey@gmail.com>
2018-02-16 08:48:49 +0100

    Fixing create items

--- public/js/data.js
+++ public/js/data.js
@@ -1,73 +1,64 @@
+axios.interceptors.request.use(
+  config => {
+    config.baseURL = "http://localhost:3000/apiv1/";
 
-var getItems = function (name, price, tags) {
-  axios.interceptors.request.use((config) => {
-    config.baseURL = "http://localhost:3000/apiv1";
-  
-    return config;  
-  }, (error) => {
+    return config;
+  },
+  error => {
     return Promise.reject(error);
-  });
+  }
+);
 
+axios.defaults.baseURL = "http://localhost:3000/apiv1/";
+
+var getItems = function(name, price, tags, pageNumber) {
   return axios.get("anuncios?limit=10", {
-    before: (e) => {
+    before: e => {
       loader.show();
     },
     params: {
       name: name,
       price: price,
-      tags: tags
+      tags: tags,
+      page: pageNumber
     }
-  })
-}
-
-var createItem = function (name, price, selling, tags, photo) {
-  var data = {
-    name: name,
-    selling: selling,
-    price: parseInt(price),
-    photo: photo,
-    tags: tags
-  };
-
-  var success = function (data) {
-    anuncios.push(data);
-    $(":input")
-      .not(":button, :submit, :reset, :hidden, :checkbox, :radio")
-      .val("");
-    drawTasks();
-  };
-
-  $.ajax({
-    type: "POST",
-    url: API_URL + "anuncios",
-    data: data,
-    success: success
-  }).fail(function (error) {
-    console.error("Error creando tarea.", error);
   });
 };
 
-var deleteItem = function (id) {
+var createItem = function(name, price, selling, tags, photo) {
+  return axios({
+    method: "post",
+    data: {
+      name: name,
+      selling: selling,
+      price: parseInt(price),
+      photo: photo,
+      tags: tags
+    }
+  });
+};
+
+var deleteItem = function(id) {
   $.ajax({
     type: "DELETE",
     url: API_URL + "tasks/" + id
   })
-    .done(function (data) {
-      tasks = $.grep(tasks, function (item) {
+    .done(function(data) {
+      tasks = $.grep(tasks, function(item) {
         return item.id != id;
       });
 
       drawTasks();
     })
-    .fail(function (error) {
+    .fail(function(error) {
       console.error("Error eliminando tarea", error);
     })
-    .always(function (object, status, error) {
+    .always(function(object, status, error) {
       console.log(object, status, error);
     });
 };
 
-var updateItem = function (id, name) {
+var updateItem = function(id, name) {
   var data = {
     name: name
   };
@@ -77,7 +68,7 @@ var updateItem = function (id, name) {
     url: API_URL + "tasks/" + id,
     data: data
   })
-    .done(function (data) {
+    .done(function(data) {
       for (var i = 0; i < tasks.length; i++) {
         if (tasks[i].id == id) {
           tasks[i].name = name;
@@ -86,7 +77,7 @@ var updateItem = function (id, name) {
 
       drawTasks();
     })
-    .fail(function (error) {
+    .fail(function(error) {
       console.error("Error actualizando tarea", error);
     });
 };
--- public/js/print_list.js
+++ public/js/print_list.js
@@ -1,96 +1,126 @@
-var loader = $(".loader");
-var newTaskInput = $("#newTaskName");
-var price = $("#price");
-var selling = $("#selling");
-var tags = $("#tags");
-var photo = $("#photo");
-var tasksContainer = $("#tasksContainer");
-var paginationContainer = $("#pagination")
+var loader;
+var newTaskInput;
+var price;
+var selling;
+var tags;
+var photo;
+var tasksContainer;
+var paginationContainer;
+var paginationItems;
 
-$(document).ready(function () {
+$(document).ready(function() {
+  var loader = $(".loader");
+  var price = $("#price");
+  var selling = $("#selling");
+  var tags = $("#tags");
+  var photo = $("#photo");
+  tasksContainer = $("#tasksContainer");
+  paginationContainer = $("#pagination");
+  paginationItems = $("a#paginationItem");
 
-    var result = getItems("", "", "")
-        .then(function (response) {
-            loader.fadeOut();
-            drawTasks(response.data);
-        })
-        .catch(function (error) {
-            console.log(error);
-        });;
-
+  var result = getItems("", "", "")
+    .then(function(response) {
+      loader.fadeOut();
+      drawTasks(response.data);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
 });
 
-$(".list-filters input").keyup(function () {
-    var filters = {
-        name: $("#filter-name").val(),
-        price: $("#filter-price").val(),
-        tags: $("#filter-tags").val()
-    };
+$(document).on("keyup", ".list-filters input", function() {
+  var filters = {
+    name: $("#filter-name").val(),
+    price: $("#filter-price").val(),
+    tags: $("#filter-tags").val(),
+    page: event.currentTarget.text
+  };
 
-    getItems(filters.name, filters.price, filters.tags);
+  var result = getItems(filters.name, filters.price, filters.tags)
+    .then(function(response) {
+      drawTasks(response.data);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
 });
 
-$("#sendNewTask").on("click", function (event) {
-    if (newTaskInput.val() != "") {
-        event.preventDefault();
-        createTask(
-            newTaskInput.val(),
-            price.val(),
-            selling.val(),
-            tags.val(),
-            photo.val()
-        );
+$(document).on("click", "#sendNewTask", function(event) {
+  var newTaskInput = $("#newTaskName");
+
+  var result = createItem(
+    newTaskInput.val(),
+    price.val(),
+    selling.val(),
+    tags.val(),
+    photo.val()
+  )
+    .then(function(response) {
+      alert("task created: " + response.data);
+      drawTasks(response.data);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+});
+
+$(document).on("click", "a#paginationItem", function(event) {
+  var filters = {
+    name: $("#filter-name").val(),
+    price: $("#filter-price").val(),
+    tags: $("#filter-tags").val(),
+    page: event.currentTarget.text
+  };
+
+  var result = getItems(filters.name, filters.price, filters.tags, filters.page)
+    .then(function(response) {
+      drawTasks(response.data);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+});
+
+$(document).on("click", ".deleteTask", function(event) {
+  var id = $(this).data("taskId");
+  deleteTask(id);
+});
+
+$(document).on("blur", ".update-task-input", function(event) {
+  var newName = $(this).val();
+  var id = $(this)
+    .siblings(".deleteTask")
+    .data("taskId");
+  updateTask(id, newName);
+});
+
+var drawTasks = function(data) {
+  var anuncios = data;
+  tasksContainer.empty();
+
+  if (anuncios.result.length == 0) {
+    tasksContainer.append(
+      '<li class="task-item">No items to be displayed!!</li>'
+    );
+  } else {
+    var contentToAdd = "";
+    var pages = anuncios.result.length / 5; // Fixed to 5 at the moment
+    var paginationElements = "";
+
+    if (paginationItems.length !== anuncios.result.length) {
+      paginationContainer.empty();
+      for (var i = 0; i < pages; i++) {
+        paginationElements += `<li><a href="#" id="paginationItem">${i +
+          1}</a></li>`;
+      }
     }
-});
 
-$("a#paginationItem").on("click", function(event){
-    var filters = {
-        name: $("#filter-name").val(),
-        price: $("#filter-price").val(),
-        tags: $("#filter-tags").val(),
-        page: event.target.value
-    };
+    paginationContainer.append(paginationElements);
 
-    getItems(filters.name, filters.price, filters.tags, page);
-});
+    for (var i = 0; i < anuncios.result.length; i++) {
+      let selling = anuncios.result[i].selling ? "On Sale!!!" : "To Rent!!!";
 
-$(document).on("click", ".deleteTask", function (event) {
-    var id = $(this).data("taskId");
-    deleteTask(id);
-});
-
-$(document).on("blur", ".update-task-input", function (event) {
-    var newName = $(this).val();
-    var id = $(this)
-        .siblings(".deleteTask")
-        .data("taskId");
-    updateTask(id, newName);
-});
-
-
-var drawTasks = function (data) {
-    var anuncios = data;
-    tasksContainer.empty();
-
-    if (anuncios.result.length == 0) {
-        tasksContainer.append(
-            '<li class="task-item">No tienes tareas pendientes</li>'
-        );
-    } else {
-        var contentToAdd = "";
-        var pages = anuncios.result.length / 5; // Fixed to 5 at the moment
-        var paginationElements = "";
-
-        for (var i = 0; i < pages; i++) {
-            paginationElements += `<li><a href="#" id="paginationItem">${i + 1}</a></li>`
-        }
-
-        paginationContainer.append(paginationElements);
-
-        for (var i = 0; i < anuncios.result.length; i++) {
-            let selling = anuncios.result[i].selling ? "On Sale!!!" : "To Rent!!!";
-
-            contentToAdd += `<div class="items-rows">
+      contentToAdd += `<div class="items-rows">
         <div class="col-md-4 list-item">
             <div class="thumbnail">
             <img src=${anuncios.result[i].photo}>
@@ -111,8 +141,8 @@ var drawTasks = function (data) {
             </div>
         </div>
     </div>`;
-        }
-
-        tasksContainer.append(contentToAdd);
     }
+
+    tasksContainer.append(contentToAdd);
+  }
 };
